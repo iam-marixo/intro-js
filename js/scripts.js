a2c98224f2aa81894a81a5512688a4869485819d
iam-marixo <marisolgarcia.11314@gmail.com>
2018-11-07 21:01:38 -0800

    daythree some notes

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// const obj1 = {
+//   name : 'Marisol'
+// };
+//
+// const obj2 = {
+//   name : 'Marisol'
+// };
+//
+// const array = ['cat'];
+// const array = ['cat'];
+
+// const age = prompt('How old are you?');
+// console.log(age >+ 21);
+
+const num = '42.3';
+console.log(num);
+console.log(+num);
