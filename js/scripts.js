faabce18d829593fb7cb8657c58e9cddbafdb5cd
iam-marixo <marisolgarcia.11314@gmail.com>
2018-11-07 20:12:02 -0800

    daytwo done

---
+++ js/scripts.js
@@ -0,0 +1,71 @@
+// var name = "Marisol"; /*string*/
+
+// var nameTwo = new String("Garcia"); /*add a new string*/
+
+// var luckyNumber = 10; /*numbers*/
+
+// var boolean = true; /*true or false*/
+
+// var mystery; /*no definition*/
+
+// console.log(name == nameTwo); /**nameTwo is a object**/
+
+
+
+// const name = "Marisol"; /*caling it that so will have two Marisol*/
+//  console.log(name);
+// {
+//    const name = "Nathan" /*nathan name is more imporant within js*/
+//   console.log(name);
+// }
+// console.log(name);
+
+// console.log("Marisol", firstName); /*custom name as firstName */
+//
+// let firstName = "Marisol";
+//
+// console.log("Marisol", firstName);
+//
+// const firstName = "Marisol";
+//
+// console.log("Marisol", firstName);
+
+// const name = "Mari";
+//
+// console.log(`Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco ${name} laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`);
+//
+// const date = new Date (''); /*give the exact date and time*/
+//
+// console.log(date);
+//
+// console.log('loreamdkjfkljfljflkjfijfsjfisudfskldjfslkdocwueifpkdsncjdnfeuwrfndfkcndfuwefiuwkf'.length); /*wanna know the length of something is length*/
+//
+// console.log(str.length);
+//
+// const num = new Number(10);
+// const num2 = 10;
+//
+// const array = [1, 3];
+// const array2 = new Array(1, 3);
+// const arrayMixed = ['Marisol', 19, true];
+//
+// console.log(arrayMixed);
+//
+// const obj = {
+//   name: 'Marisol',
+//   age: 19;
+// };
+// console.log(obj);
+
+
+// alert ('Danger will come among us')
+
+// const response = confirm('Are you sure?');
+//
+// console.log(response);
+
+const favNum = prompt('What is your favorite number?');
+const realNum = parseInt(favNum);
+const num = 44;
+
+console.log(parseInt(num));
