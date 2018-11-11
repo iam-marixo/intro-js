15374f30cb080fce85cf2871c8a8c74bb8f0c3cd
iam-marixo <marisolgarcia.11314@gmail.com>
2018-11-10 17:56:43 -0800

    finished daytwo homework

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
-const firstName = prompt('What is your first name?');
-/*users first name*/
+const firstName = prompt('What is your first name?');/*users first name*/
 const lastName = prompt('What is your last name?');/*users last name*/
-console.log(firstName);
 
-alert ('lastName');
+console.log(firstName);/*logs users first name in console*/
 
-const birthday = new Date(prompt('What is your birthday?'));
-confirm(`Is your birthday correct: $[birthday]`);
+alert (lastName);/*alert for last name*/
 
-alert(`You birthday is: $[birthday]`);
+const birthday = new Date(prompt('What is your birthday?'));/*asks users for birthday*/
+confirm(`Is your birthday correct: ${birthday}`);/*confirmation for users birthday*/
+
+alert(`You birthday is: ${birthday}`);/*displays users birthday*/
