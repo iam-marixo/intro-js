26c49c5bcf70ff8a99914b6bbfebd4847dea5422
iam-marixo <marisolgarcia.11314@gmail.com>
2018-11-13 22:18:53 -0800

    finish takehome-day3 homework

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+  var firstName = prompt('What is your first name?', 'enter first name');
+  var favColor = prompt(`${firstName}, What is your favorite color out of the colors shown?, colors: purple, green, yellow, blue, orange, pink, white, grey, or black?`);
+   switch (favColor) {
+
+     case 'purple':
+      alert(`${favColor}, you choose purple! Did you know purple represents a calming mind, peace, or wealth? ${firstName}!`);
+       break;
+
+     case 'green':
+      alert(`${firstColor}, you choose green! Did you know green represents growing or renewing? ${firstName}!`);
+       break;
+
+     case 'yellow':
+        alert(`${favColor}, your choose green! Did you know yellow represents happiness or energy? ${firstName}!`);
+         break;
+
+     case 'blue':
+        alert(`${favColor}, you choose blue! Did you know blue represents stability or orderly. ${firstName}!`);
+         break;
+
+     case 'orange':
+        alert(`${favColor}, you choose orange! Did you know orange represents joy or warmth!? ${firstName}!`);
+         break;
+
+     case 'pink':
+       alert(`${favColor}, you choose pink! Did you know pink represents playful or romantic? ${firstName}!`);
+        break;
+
+     case 'white':
+       alert(`${favColor}, you choose white! Did you know white represents complete or at pure? ${firstName}!`);
+       break;
+
+     case 'grey':
+       alert(`${favColor}, You choose grey! Did you know grey represents emotionless or formal ${firstName}!`);
+       break;
+
+       case 'black':
+       alert(`${favColor}, You choose black! Did you know black represents death or mystery!? ${firstName}!`);
+         break;
+
+   }
+  var firstName = prompt("Hello?! you didnt enter your name! What is your first name?", "enter first name");
