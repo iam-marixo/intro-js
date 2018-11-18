fd8bb61e37e7db4e939365d14117cc6056b78a4a
iam-marixo <marisolgarcia.11314@gmail.com>
2018-11-17 22:35:47 -0800

    COMPLETELY LOST ON HOME :(

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+const = ();
+user.list = prompt('What do you need to do this weekend?');
+user.listTime = +prompt('How long will each todo take?');
+
+console.log(list);
+console.log(listTime);;
+
+// const weekendTodoList = ['Grab a dish for church dinner', 'Brother bday gift', 'Get food for weekend'];
+//
+// console.log(weekendTodoList);;
