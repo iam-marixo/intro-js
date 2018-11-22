dd2dc18d1a4bb721ded89e582b6a34122de55fa7
iam-marixo <marisolgarcia.11314@gmail.com>
2018-11-21 20:04:50 -0800

    daysix notes

---
+++ js/scripts.js
@@ -0,0 +1,47 @@
+// const body = document.querySelectorAll('.item');
+// console.log(body);
+//
+// function query(queryStr) {
+//   console.log(document.querySelector(queryStr));
+// }
+//
+// query('body');
+
+// const header = document.querySelector('#header');
+// console.log(header);
+//
+// header.addEventListener('click', event => header.style.display = 'none');
+//
+// document.addEventListener('mouseover', event => {
+//   console.log(event);
+//
+//   if(event.key === 'Enter') {
+//     alert('You hit the enter key.');
+//   }
+//   header.style.color = 'tomato';
+// });
+
+// const header = document.querySelector('#header');
+// const container = document.querySelector('#container');
+// const btn = document.querySelector('#btn');
+//
+// btn.addEventListener('click', () => alert ('Buttton clicked'));
+// container.addEventListener('mouseover', () => container.style.background = 'purple');
+// document.addEventListener('keyup', () => header.style.background = 'green');
+// // document.addEventListener('click', () => window.location.assign ('http://www.google.com'));
+//
+// const buttons = document.querySelectorAll('button');
+// console.log(buttons);
+//
+// function.someFunction(event);
+// console.log(event);
+// }
+//
+// for (let button of buttons) {
+//   button.addEventListener('click', event => someFunction());
+//
+// buttons.forEach(button => button.addEventListener('click', () => alert('clicked')));
+
+
+input.value = 26;
+ convertor look up on google
